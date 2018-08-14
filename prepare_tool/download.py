bc47adf3b3b9ceadf0783c73a8f959c0e2117c4a
3846masa <3846masahiro+git@gmail.com>
2018-08-14 21:26:40 +0900

    Add referer header

--- prepare_tool/download.py
+++ prepare_tool/download.py
@@ -3,9 +3,11 @@ import requests
 from pathlib import Path
 from urllib.parse import urlparse
 
+from prepare_tool.package_info import PackageInfo
 
-def downloadFileFromUrl(url: str, outDir: Path):
-    res = requests.get(url)
+
+def downloadFileFromUrl(url: str, outDir: Path, package_info: PackageInfo):
+    res = requests.get(url, headers={'Referer': package_info.website})
     _, params = cgi.parse_header(res.headers.get('content-disposition', ''))
     file_name = params.get('filename', Path(urlparse(res.url).path).name)
     file_path = outDir.joinpath(file_name)
